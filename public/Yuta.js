998bfbc8b1fb14bd6d2d7882c67bc4b1fc5a1642
zilong <jzlxiaohei@163.com>
2016-03-17 17:12:31 +0800

    yuta test

--- public/Yuta.js
+++ public/Yuta.js
@@ -1,30 +1,31 @@
-!function(exports){
+!function (exports) {
 
-    function noop(){}
+    function noop() {
+    }
 
     var callbackIndex = 1
 
     var callBackFns = []
 
-    exports['__YutaAppCallback'] = function(cdId,args){
+    exports['__YutaAppCallback'] = function (cdId, args) {
         callBackFns[cdId](args)
     }
 
 
-    function invokeWrap(methodName,args,fn){
-        args = args||{}
+    function invokeWrap(methodName, args, fn) {
+        args = args || {}
         fn = fn || noop
-        if('callbackId' in args){
+        if ('callbackId' in args) {
             throw new Error(' `callbackId` in args!! should not do this')
         }
         var id = callbackIndex.toString();
         args['callbackId'] = id
         callBackFns[id] = fn
         callbackIndex++;
-        __YutaJsBridge.invoke(methodName,JSON.stringify(args))
+        __YutaJsBridge.invoke(methodName, JSON.stringify(args))
     }
 
-    function defaultFailFunc(e){
+    function defaultFailFunc(e) {
         console.warn(e)
     }
 
@@ -34,24 +35,24 @@
      * onFail:function,
      * args:{}
      */
-    function wrapSuccessFailFunc(options){
+    function wrapSuccessFailFunc(options) {
         var methodName = options.methodName,
             args = options.args
-        if(!methodName){
+        if (!methodName) {
             throw new Error('methodName is required')
         }
-        if(typeof options.onSuccess !== 'function'){
+        if (typeof options.onSuccess !== 'function') {
             throw new Error('onSuccess is required')
         }
         options.onFail = options.onFail || defaultFailFunc()
 
-        invokeWrap(options.methodName,args,function(args){
+        invokeWrap(options.methodName, args, function (args) {
             var msg = args['message']
-            if(msg == Constant.MsgSuccess){
+            if (msg == Constant.MsgSuccess) {
                 options.onSuccess(args[Constant.Results])
-            }else if(msg == Constant.MsgFail){
+            } else if (msg == Constant.MsgFail) {
                 options.onFail(args[Constant.Results])
-            }else{
+            } else {
                 console.warn('unknown message type ')
             }
         })
@@ -59,22 +60,22 @@
     }
 
 
-    var Constant={
-        MsgSuccess :'success',
-        MsgFail:'fail',
-        Results:'results',
-        Camera:{
-            Direction:{
-                Back:0,
-                Front:1
+    var Constant = {
+        MsgSuccess: 'success',
+        MsgFail: 'fail',
+        Results: 'results',
+        Camera: {
+            Direction: {
+                Back: 0,
+                Front: 1
             },
-            DestinationType:{
-                DATA_URL:0,
-                FILE_URI:1
+            DestinationType: {
+                DATA_URL: 0,
+                FILE_URI: 1
             },
-            EncodingType:{
-                JPEG:0,
-                PNG:1
+            EncodingType: {
+                JPEG: 0,
+                PNG: 1
             }
         }
     }
@@ -85,33 +86,59 @@
     var Yuta = {}
 
     Yuta.Constant = Constant
-    Yuta.Camera={
-        getPicture:function(onSuccess,onFail,options){
+    Yuta.Camera = {
+        getPicture: function (onSuccess, onFail, options) {
             wrapSuccessFailFunc({
-                methodName:'Yuta.Camera.getPicture',
-                onSuccess:onSuccess,
-                onFail:onFail,
-                args:options})
+                methodName: 'Yuta.Camera.getPicture',
+                onSuccess: onSuccess,
+                onFail: onFail,
+                args: options
+            })
         }
     }
 
-    Yuta.WebView={
-        close:function(url){
-            invokeWrap('Yuta.WebView.close',url)
+    Yuta.WebView = {
+        close: function (url) {
+            invokeWrap('Yuta.WebView.close', {url: url})
         },
-        closeAll:function(url){
-            invokeWrap('Yuta.WebView.closeAll',url)
+        closeAll: function (url) {
+            invokeWrap('Yuta.WebView.closeAll', {url: url})
         },
-        load:function(url){
-            invokeWrap('Yuta.WebView.load',url)
+        load: function (url) {
+            invokeWrap('Yuta.WebView.load', {url: url})
         },
-        open:function(url){
-            invokeWrap('Yuta.WebView.open',url)
+        open: function (url) {
+            invokeWrap('Yuta.WebView.open', {url: url})
         }
     }
 
 
-    Yuta.Share
+    Yuta.Share = {
+        image: function (image, url, title, content, callback) {
+            invokeWrap('Yuta.Share.image', {
+                    image: image,
+                    url: url,
+                    title: title,
+                    content: content
+                }, callback
+            )
+        },
+        video: function (url, thumb, title, callback) {
+            invokeWrap('Yuta.Share.video', {
+                url: url,
+                thumb: thumb,
+                title: title
+            }, callback)
+        },
+        music: function (url, thumb, title, author, callback) {
+            invokeWrap('Yuta.Share.music', {
+                url: url,
+                thumb: thumb,
+                title: title,
+                author: author
+            }, callback)
+        }
+    }
 
     exports.Yuta = Yuta
 }(window)
