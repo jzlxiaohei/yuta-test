264c864dc1984ca239679467a4d6eb0f8c9ac6d2
zilong <jzlxiaohei@163.com>
2016-03-17 13:31:03 +0800

    fixed type

---
+++ Yuta.js
@@ -0,0 +1,81 @@
+!function(exports){
+    var callbackIndex = 1
+
+    var callBackFns = []
+
+    exports['__YutaAppCallback'] = function(cdId,args){
+        callBackFns[cdId](args)
+    }
+
+    function invokeWrap(methodName,args,fn){
+        if('callbackId' in args){
+            throw new Error(' `callbackId` in args!! should not do this')
+        }
+        var id = callbackIndex.toString();
+        args['callbackId'] = id
+        callBackFns[id] = fn
+        callbackIndex++;
+        __YutaJsBridge.invoke(methodName,args)
+    }
+
+    function defaultFailFunc(e){
+        console.warn(e)
+    }
+
+    /**
+     * @param options:
+     * onSuccess:function required
+     * onFail:function,
+     * args:{}
+     */
+    function generateSuccessFailFunc(options){
+        options.onFail = options.onFail || defaultFailFunc()
+        return function (){
+            invokeWrap('Yuta.Camera.getPicture',options,function(args){
+                var msg = args['message']
+                if(msg == Constant.MsgSuccess){
+                    onSuccess(args[Constant.Results])
+                }else if(msg == Constant.MsgFail){
+                    onFail(args[Constant.Results])
+                }else{
+                    console.warn('unknown message type ')
+                }
+            })
+        }
+    }
+
+
+    var Constant={
+        MsgSuccess :'success',
+        MsgFail:'fail',
+        Results:'results',
+        Camera:{
+            Direction:{
+                Back:1
+            }
+        }
+    }
+
+    /**
+     * Yuta Object
+     */
+    var Yuta = {}
+
+
+
+    Yuta.Camera={
+        getPicture:function(onSuccess,onFail,options){
+            invokeWrap('Yuta.Camera.getPicture',options,function(args){
+                var msg = args['message']
+                if(msg == Constant.MsgSuccess){
+                    onSuccess(args[Constant.Results])
+                }else if(msg == Constant.MsgFail){
+                    onFail(args[Constant.Results])
+                }else{
+                    console.warn('unknown message type ')
+                }
+            })
+        }
+    }
+
+}(window)
